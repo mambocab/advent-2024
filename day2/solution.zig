37deaf94b03635206b2e6b908927da018a62e659
Jim Witschey <jim.witschey@gmail.com>
2024-12-02 08:44:53 -0500

    day 2 init

---
+++ day2/solution.zig
@@ -0,0 +1,6 @@
+// File structure bitten from https://kristoff.it/blog/advent-of-code-zig/
+const std = @import("std");
+const input = @embedFile("input");
+const example = @embedFile("example");
+
+pub fn main() !void {}
