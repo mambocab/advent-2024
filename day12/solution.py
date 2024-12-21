d76047b430a58e262e3031d013ce37d2e241b0e0
Jim Witschey <jim.witschey@gmail.com>
2024-12-20 21:02:01 -0500

    day 12 wip

--- day12/solution.py
+++ day12/solution.py
@@ -1,3 +1,4 @@
+from functools import cache
 from itertools import product
 
 example_small = """
@@ -20,9 +21,40 @@ MIIISIJEEE
 MMMISSJEEE
 """.strip()
 
+type Grid = tuple[str, ...]
+
+
+@cache
+def get_from_grid(point, grid):
+    try:
+        grid[point[1]][point[0]]
+    except IndexError:
+        return None
+
+@cache
+def neighbors(point, grid: Grid):
+    return set(
+        candidate
+        for candidate in (
+            (point[0] - 1, point[1]),
+            (point[0], point[1] - 1),
+            (point[0] + 1, point[1]),
+            (point[0], point[1] + 1),
+        )
+        if get_from_grid(candidate, grid) is not None
+    )
 
 def test_regionize_example_small():
-    lines = example_small.splitlines()
+    lines: Grid = tuple(example_small.splitlines())
     rows, cols = len(lines), len(lines[0])
     coords = set(product(range(cols), range(rows)))
     seen = set()
+    while seen != coords:
+        for point in coords - seen:
+            if point in seen:
+                continue
+            
+            seen.add(point)
+            at_point = get_from_grid(point(lines))
+            for neighbor in neighbors(point, lines):
+                if ...
