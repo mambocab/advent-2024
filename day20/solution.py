da4e069c5722c6ccbe2d6ed4cb9f4d62faa4c1e9
Jim Witschey <jim.witschey@gmail.com>
2024-12-20 19:40:10 -0500

    day 20 wip

--- day20/solution.py
+++ day20/solution.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from enum import Enum, auto
 from functools import cache
-from typing import assert_never
+import pytest
 
 example = """
 ###############
@@ -35,14 +35,9 @@ class Square(Enum):
 
 
 class CheatingState(Enum):
-    NOT_STARTED = auto()
+    NO = auto()
     STARTED = auto()
     DONE = auto()
-
-
-@cache
-def at_point(point, grid) -> Square:
-    return grid[point[1]][point[0]]
 
 
 @dataclass
@@ -96,21 +91,114 @@ class Map:
                 elif char == Square.TRACK:
                     builder.append(".")
             builder.append("\n")
-        return ''.join(builder[:-1])
+        return "".join(builder[:-1])
 
-def test_parsing():
-    assert repr(Map.from_str(example)) == example
+
+@pytest.fixture
+def parsed_example() -> Map:
+    return Map.from_str(example)
+
+
+@pytest.fixture
+def read_input() -> str:
+    with open("input") as f:
+        return f.read().strip()
+
+
+@pytest.fixture
+def parsed_input(read_input: str) -> Map:
+    return Map.from_str(read_input)
+
+
+class TestParsing:
+    def test_parsing(self, parsed_example: Map):
+        assert repr(parsed_example) == example
+
+    def test_parsed_input(self, parsed_input: Map, read_input: str):
+        assert repr(parsed_input) == read_input
+
+
+@cache
+def at_point(point: Point, grid: Grid) -> Square | None:
+    try:
+        return grid[point[1]][point[0]]
+    except IndexError:
+        return None
+
 
 @cache
 def cheating_state(path: Path, grid: Grid) -> CheatingState:
     if len(path) == 0:
-        return CheatingState.NOT_STARTED
+        return CheatingState.NO
 
     currently_cheating = False
     for point in path:
-        if grid[point[1]][point[0]] == Square.WALL:
+        floor = at_point(point=point, grid=grid)
+        if floor == Square.WALL:
             if currently_cheating:
                 return CheatingState.DONE
             currently_cheating = True
+        elif floor == Square.TRACK:
+            if currently_cheating:
+                return CheatingState.DONE
 
-    return CheatingState.NOT_STARTED
+    return CheatingState.STARTED if currently_cheating else CheatingState.NO
+
+
+class TestCheatingState:
+    def test_not_cheating_trivial(self, parsed_example: Map):
+        assert cheating_state(tuple(), parsed_example.grid) == CheatingState.NO
+
+    def test_not_cheating(self, parsed_example: Map):
+        #       start,  ^     , ^     , >     , >
+        path = ((1, 3), (1, 2), (1, 1), (2, 1), (3, 1))
+        assert cheating_state(path[:1], parsed_example.grid) == CheatingState.NO
+        assert cheating_state(path[:2], parsed_example.grid) == CheatingState.NO
+        assert cheating_state(path[:3], parsed_example.grid) == CheatingState.NO
+        assert cheating_state(path, parsed_example.grid) == CheatingState.NO
+
+    def test_mid_cheat(self, parsed_example: Map):
+        #       start,  ^     , >
+        path = ((1, 3), (1, 2), (2, 2))
+        assert cheating_state(path[:1], parsed_example.grid) == CheatingState.NO
+        assert cheating_state(path[:2], parsed_example.grid) == CheatingState.NO
+        assert cheating_state(path, parsed_example.grid) == CheatingState.STARTED
+
+    def test_already_cheated_single_step(self, parsed_example: Map):
+        #       start,  >     , >
+        path = ((1, 3), (2, 3), (3, 3))
+        assert cheating_state(path[:1], parsed_example.grid) == CheatingState.NO
+        assert cheating_state(path[:2], parsed_example.grid) == CheatingState.STARTED
+        assert cheating_state(path, parsed_example.grid) == CheatingState.DONE
+
+    def test_already_cheated_two_steps(self, parsed_example: Map):
+        #       start,  >     , ^     , ^
+        path = ((1, 3), (2, 3), (2, 2), (2, 1))
+        assert cheating_state(path[:1], parsed_example.grid) == CheatingState.NO
+        assert cheating_state(path[:2], parsed_example.grid) == CheatingState.STARTED
+        assert cheating_state(path[:3], parsed_example.grid) == CheatingState.DONE
+        assert cheating_state(path, parsed_example.grid) == CheatingState.DONE
+
+
+def neighbors(point: Point, grid: Grid, already_cheated: bool) -> set[Point]:
+    candidates = (
+        (point[0] - 1, point[1]),
+        (point[0], point[1] - 1),
+        (point[0] + 1, point[1]),
+        (point[0], point[1] + 1),
+    )
+
+    def valid(s: Square | None) -> bool:
+        if already_cheated:
+            return s == Square.TRACK
+        return s is not None
+
+    return set(
+        candidate for candidate in candidates if valid(at_point(candidate, grid))
+    )
+
+
+def find_paths(m: Map): ...
+
+
+def test_example_counts(parsed_example): ...
